aac60350ddeabe40669ecd8f162e7ee653c67fb2
João Moreira <joaomoreirarafael@gmail.com>
2025-04-30 15:54:14 +0100

    Update micropython.py

--- examples/micropython.py
+++ examples/micropython.py
@@ -6,6 +6,8 @@ mqttsn = ESP32_MQTTSN_BG95(uart_id=1, baudrate=115200, tx=17, rx=16)
 mqttsn.connect_apn("1", "internet.apn")
 mqttsn.apn_verify()
 
+mqttsn.conf_ntn(1,"apn_name")
+
 mqttsn.connect_broker("client1", "mqtt.example.com", "1883")
 mqttsn.broker_verify()
 
